212e5de11a1cfec9827450a5b920abc5aa514f74
Katie Bittner <katiebittner1@gmail.com>
2020-10-14 13:22:42 -0400

    controller methods and routes

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -1,2 +1,15 @@
 class CourtsController < ApplicationController
+
+  def index
+    @courts = Court.all
+
+    render json: @courts, include: :players
+  end
+
+  def show
+    @court = Court.find(params[:id])
+
+    render json: @court, include: :players
+  end
+
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,25 @@
 class PlayersController < ApplicationController
+
+  def index
+    @players = Player.all
+
+    render json: @players, include: :court
+  end
+
+  def show
+    @player= Player.find(params[:id])
+
+    render json: @player, include: :court
+  end
+
+  def create
+    @player = Player.create(name: params[:name],
+      age: params[:age], gender: params[:gender],
+      skill: params[:skill], phone: params[:phone],
+      court_id: params[:court_id])
+
+
+    render json: @player
+  end
+
 end
